23007f734c321abf04abc21f1f1d415b8486edf8
Árpád Móczó <arpad.moczo.ifj@gmail.com>
2019-12-01 10:20:21 +0100

    Resource file update 2.0

--- UserMaintenance/UserMaintenance/Resource.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource.Designer.cs
@@ -72,18 +72,18 @@ namespace UserMaintenance {
         /// <summary>
         ///   Looks up a localized string similar to Utónév.
         /// </summary>
-        internal static string FirstName {
+        internal static string FullName {
             get {
-                return ResourceManager.GetString("FirstName", resourceCulture);
+                return ResourceManager.GetString("FullName", resourceCulture);
             }
         }
         
         /// <summary>
         ///   Looks up a localized string similar to Vezetéknév.
         /// </summary>
-        internal static string LastName {
+        internal static string WriteInFile {
             get {
-                return ResourceManager.GetString("LastName", resourceCulture);
+                return ResourceManager.GetString("WriteInFile", resourceCulture);
             }
         }
     }
